2bc4a96ff6bb6e32dfa8e49fd6e4364777e6182c
Cole Titze <cole.titze@outlook.com>
2022-09-03 21:24:35 -0500

    Removed azure

--- VegasOddsGetterTrigger/trigger.py
+++ VegasOddsGetterTrigger/trigger.py
@@ -4,20 +4,9 @@ import time
 import math
 import VegasOddsGetter.vegas_odds_getter as odds_getter
 import DataAccess.data_access as da
-import azure.functions as func
 import Entities.vegas_enums as vegas
 SportsBooks = vegas.SportsBook
 Odds = vegas.OddsType
-
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    trigger()
 
 # Foreach sportsbook get the opening and closing odds for a set number of games
 # Save the games after each odds for each sportsbook
