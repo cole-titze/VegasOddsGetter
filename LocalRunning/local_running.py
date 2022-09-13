211fb8014ee6cc2bdfc096e23731ff2e104e8d9d
Cole Titze <cole.titze@outlook.com>
2022-09-12 20:52:19 -0500

    Added passing if local settings does not exist

--- LocalRunning/local_running.py
+++ LocalRunning/local_running.py
@@ -4,12 +4,16 @@ import os
 
 # Locally set secrets, when in azure these will already be set
 def set_local_environment():
-    with open("local.settings.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()
-    os.environ["SQL_DATABASE"] = jsonObject["Values"]["SQL_DATABASE"]
-    os.environ["SQL_USERNAME"] = jsonObject["Values"]["SQL_USERNAME"]
-    os.environ["SQL_PASSWORD"] = jsonObject["Values"]["SQL_PASSWORD"]
+    # If the local file doesn't exist assume using env variables previously set
+    try:
+        with open("local.settings.json") as jsonFile:
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
+        os.environ["SQL_DATABASE"] = jsonObject["Values"]["SQL_DATABASE"]
+        os.environ["SQL_USERNAME"] = jsonObject["Values"]["SQL_USERNAME"]
+        os.environ["SQL_PASSWORD"] = jsonObject["Values"]["SQL_PASSWORD"]
+    except Exception:
+        pass
 
 set_local_environment()
 trigger.trigger()
